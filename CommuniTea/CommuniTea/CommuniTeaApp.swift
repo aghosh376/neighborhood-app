58bbe3e6eed6f3e578e26687ce4e52a247a31331
aghosh376 <arayboghosh@gmail.com>
2025-05-20 23:54:59 -0700

    Update README.md

---
+++ CommuniTea/CommuniTea/CommuniTeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommuniTeaApp.swift
+//  CommuniTea
+//
+//  Created by Amartya Raybo Ghosh on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct CommuniTeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
